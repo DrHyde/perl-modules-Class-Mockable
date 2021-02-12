df5a1f84270be938ebe1f5ce013d987c8d16d64d
David Cantrell <david@cantrell.org.uk>
2021-02-12 18:44:03 +0000

    temporarily remove doco for recording of fixtures, which isn't yet implemented

--- lib/Class/Mock/Method/InterfaceTester.pm
+++ lib/Class/Mock/Method/InterfaceTester.pm
@@ -190,8 +190,14 @@ the right class (again, not a subclass), or 'invocant_object' subref to
 check that it's being called on an object that, when passed to the sub-ref,
 returns true.
 
-Alternatively, C<new> can read fixture data from a file or you can use it
-to pass args through to other code and record its responses to a file.
+Alternatively, C<new> can read fixture data from a file.
+
+Recording fixtures to a file is not yet implemented.
+
+=cut
+
+# or you can use it
+# to pass args through to other code and record its responses to a file.
 
 =over
 
@@ -206,29 +212,33 @@ Yes, that's a reference to a scalar. The scalar is assumed to be a filename
 which will be read, and whose contents should be valid arguments to create
 fixtures.
 
-=item recording and writing to a file
+=cut
 
-Set the environment varilable PERL_CMMIT_RECORD to a true value and Pass a
-reference to a scalar as the first argument, following by the name of class
-whose interactions you want to record, and optionally either a list of method
-names or a regular expression matching some method names:
-
-    Class::Mock::Method::InterfaceTester->new(
-        \"filename.dd",
-        'I::Want::To::Mock::This',
-        qw(but only these methods) # or qr/^(but|only|these|methods)$/
-    );
-
-In the absence of a list of methods (or a regex) then all methods will be
-recorded, including those inherited from superclasses, except those whose names
-begin with an underscore.
+# =item recording and writing to a file
+# 
+# Set the environment varilable PERL_CMMIT_RECORD to a true value and Pass a
+# reference to a scalar as the first argument, following by the name of class
+# whose interactions you want to record, and optionally either a list of method
+# names or a regular expression matching some method names:
+# 
+#     Class::Mock::Method::InterfaceTester->new(
+#         \"filename.dd",
+#         'I::Want::To::Mock::This',
+#         qw(but only these methods) # or qr/^(but|only|these|methods)$/
+#     );
+# 
+# In the absence of a list of methods (or a regex) then all methods will be
+# recorded, including those inherited from superclasses, except those whose names
+# begin with an underscore.
 
 =back
 
-The observant amongst you will have noticed that because when reading from
-a file all arguments after the first are ignored, then you can choose to
-record or to playback by just setting the environment variable and making
-no other changes. This is deliberate.
+=cut
+
+# The observant amongst you will have noticed that because when reading from
+# a file all arguments after the first are ignored, then you can choose to
+# record or to playback by just setting the environment variable and making
+# no other changes. This is deliberate.
 
 =head1 SEE ALSO
 
